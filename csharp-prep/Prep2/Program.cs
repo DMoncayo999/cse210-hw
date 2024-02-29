998a028382a36166fcdacac8c69681d4415d6639
DMoncayo999 <delfina.moncayo@gmail.com>
2024-02-28 21:13:34 -0500

    Finish Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int percentage = int.Parse(userAnswer);
+
+        char letter;
+
+        if (percentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (percentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (percentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (percentage >= 60)
+        {
+            letter = 'D';
+        }
+        else 
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Great! You passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up, next time you will do better!" );
+        }        
+
     }
 }
