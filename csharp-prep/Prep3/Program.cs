3bde19168cdcd3f2d97ada89e8037efdbbc0e9cc
DMoncayo999 <delfina.moncayo@gmail.com>
2024-02-28 22:33:00 -0500

    Finish Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       // Console.Write("What is the magic number? ");
+       // int answer = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != answer)
+        {
+            Console.Write("What is your guest? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (answer > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (answer < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.Write("Yeah, you guessed it!");
+
+            }
+        }
+
     }
 }
