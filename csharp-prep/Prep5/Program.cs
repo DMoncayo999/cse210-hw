2c1d41d819c8c5c4a1b87e98edecc3eecfdd65a4
DMoncayo999 <delfina.moncayo@gmail.com>
2024-02-29 14:34:17 -0500

    Finish Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        Console.WriteLine($"Hello, {name}!");
+
+        int number = PrompUserNumber();
+        Console.WriteLine($"Your favorite number is {number}");
+
+        int square = SquareNumber(number);
+
+        DisplayResult(name, square);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PrompUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {square}");
+
+    }
+
 }
