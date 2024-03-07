15910ffc8e46dd7163ca1824522999f20a10227e
DMoncayo999 <delfina.moncayo@gmail.com>
2024-03-06 23:53:45 -0500

    working in file Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        // interface for the user
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Please select one of the following options: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    WriteNewEntry(journal, promptGenerator);
+                    break;
+                case "2":
+                    journal.DisplayAll();
+                    break;
+                case "3":
+                    LoadJournal(journal);
+                    break;
+                case "4":
+                    SaveJournal(journal);
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+
+    }
+    static void WriteNewEntry(Journal journal, PromptGenerator promptGenerator)
+    {
+        string prompt = promptGenerator.GetRandomPrompt();
+        Console.WriteLine("Prompt: " + prompt);
+        Console.Write("Your response: ");
+        string response = Console.ReadLine();
+        Entry newEntry = new Entry(prompt, response, DateTime.Now);
+        journal.AddEntry(newEntry);
+    }
+
+    static void SaveJournal(Journal journal)
+    {
+        Console.Write("Enter filename to save journal: ");
+        string saveFile = Console.ReadLine();
+        journal.SaveToFile(saveFile);
+    }
+
+    static void LoadJournal(Journal journal)
+    {
+        Console.Write("Enter filename to load journal: ");
+        string loadFile = Console.ReadLine();
+        journal.LoadFromFile(loadFile);
     }
 }
